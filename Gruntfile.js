d88e854443f9613169b32705abf20bfd1a49345e
Patrick Connolly <patrick@myplanetdigital.com>
2013-12-03 00:12:50 -0500

    Added grunt-release support.

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-release');
+
+  grunt.initConfig({
+    release: {
+      options: {
+        tagName: 'v<%= version %>',
+        commitMessage: 'Prepared to release <%= version %>.'
+      }
+    },
+  });
+
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+};
